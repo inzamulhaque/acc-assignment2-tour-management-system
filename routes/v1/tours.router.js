6a823d70bd7a1f9680a59ed6746fcf99ab1f1b67
inzamulhaque <mdihalif@gmail.com>
2022-09-20 00:50:07 +0600

    get trending tours

--- routes/v1/tours.router.js
+++ routes/v1/tours.router.js
@@ -2,10 +2,13 @@ const express = require("express");
 const createTour = require("../../controller/tours/createTours");
 const getAllTours = require("../../controller/tours/getAllTours");
 const getOneTourById = require("../../controller/tours/getOneTourById");
+const getTrending = require("../../controller/tours/getTrending");
 const updateById = require("../../controller/tours/updateById");
 const router = express.Router();
 
 router.route("/").get(getAllTours).post(createTour);
+
+router.route("/trending").get(getTrending);
 
 router.route("/:id").get(getOneTourById).patch(updateById);
 
