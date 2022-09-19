da257ab0af1a3155c6f80bd8b888680b85175a3e
inzamulhaque <mdihalif@gmail.com>
2022-09-20 00:40:15 +0600

    tour updated

--- routes/v1/tours.router.js
+++ routes/v1/tours.router.js
@@ -2,10 +2,11 @@ const express = require("express");
 const createTour = require("../../controller/tours/createTours");
 const getAllTours = require("../../controller/tours/getAllTours");
 const getOneTourById = require("../../controller/tours/getOneTourById");
+const updateById = require("../../controller/tours/updateById");
 const router = express.Router();
 
 router.route("/").get(getAllTours).post(createTour);
 
-router.route("/:id").get(getOneTourById);
+router.route("/:id").get(getOneTourById).patch(updateById);
 
 module.exports = router;
