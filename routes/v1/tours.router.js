25f95be3dc1dad5b90c18ec6fae4246ebdbdeac8
inzamulhaque <mdihalif@gmail.com>
2022-09-20 01:15:45 +0600

    Get top 3 cheapest tours

--- routes/v1/tours.router.js
+++ routes/v1/tours.router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const createTour = require("../../controller/tours/createTours");
 const getAllTours = require("../../controller/tours/getAllTours");
+const getCheapest = require("../../controller/tours/getCheapest");
 const getOneTourById = require("../../controller/tours/getOneTourById");
 const getTrending = require("../../controller/tours/getTrending");
 const updateById = require("../../controller/tours/updateById");
@@ -9,6 +10,7 @@ const router = express.Router();
 router.route("/").get(getAllTours).post(createTour);
 
 router.route("/trending").get(getTrending);
+router.route("/cheapest").get(getCheapest);
 
 router.route("/:id").get(getOneTourById).patch(updateById);
 
