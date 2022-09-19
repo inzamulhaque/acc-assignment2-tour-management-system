e80a007fd9a4d61573bf40cee6374aef7281ad7e
inzamulhaque <mdihalif@gmail.com>
2022-09-19 21:49:44 +0600

    get all tours

--- routes/v1/tours.router.js
+++ routes/v1/tours.router.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const createTour = require("../../controller/tours/createTours");
+const getAllTours = require("../../controller/tours/getAllTours");
 const router = express.Router();
 
-router.route("/").get().post(createTour);
+router.route("/").get(getAllTours).post(createTour);
 
 module.exports = router;
